6fda4524a472184855bc70df1e6f06728a6ddb1c
LSouberbielle <lucianosouberbielle@mi.unc.edu.ar>
2023-02-04 02:08:42 -0300

    Game Manager script added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+sing System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
